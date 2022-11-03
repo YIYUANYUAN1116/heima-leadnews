5cf027a21d85b1d1c23d7793bfbfa69ca96f1d62
YIYUANYUAN1116 <571145417@qq.com>
2022-11-03 21:48:49 +0800

    素材列表查询

--- heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/service/impl/WmMaterialServiceImpl.java
+++ heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/service/impl/WmMaterialServiceImpl.java
@@ -1,9 +1,13 @@
 package com.heima.wemedia.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.heima.common.autoconfig.template.MinIOTemplate;
 import com.heima.common.thread.WmThreadLocal;
+import com.heima.model.common.dtos.PageResponseResult;
 import com.heima.model.common.dtos.ResponseResult;
+import com.heima.model.wemedia.dtos.WmMaterialDto;
 import com.heima.model.wemedia.pojos.WmMaterial;
 import com.heima.wemedia.mapper.WmMaterialMapper;
 import com.heima.wemedia.service.WmMaterialService;
@@ -21,6 +25,11 @@ public class WmMaterialServiceImpl extends ServiceImpl<WmMaterialMapper, WmMater
     @Autowired
     private MinIOTemplate minIOTemplate;
 
+    /**
+     * 上传图片
+     * @param multipartFile
+     * @return
+     */
     @Override
     public ResponseResult uploadPicture(MultipartFile multipartFile) {
 
@@ -49,5 +58,32 @@ public class WmMaterialServiceImpl extends ServiceImpl<WmMaterialMapper, WmMater
         //3.返回结果
 
         return ResponseResult.okResult(wmMaterial);
+    }
+
+    /**
+     * 素材列表查询
+     * @param dto
+     * @return
+     */
+    @Override
+    public ResponseResult findList(WmMaterialDto dto) {
+        Page page = new Page(dto.getPage(), dto.getSize());
+        LambdaQueryWrapper<WmMaterial> lambdaQueryWrapper = new LambdaQueryWrapper<>();
+
+        //是否收藏
+        if (dto.getIsCollection() != null && dto.getIsCollection()==1){
+            lambdaQueryWrapper.eq(WmMaterial::getIsCollection,1);
+        }
+
+        //按照用户查
+        lambdaQueryWrapper.eq(WmMaterial::getUserId,WmThreadLocal.getUser().getId());
+
+        //按照时间倒序
+        lambdaQueryWrapper.orderByDesc(WmMaterial::getCreatedTime);
+
+        page = page(page, lambdaQueryWrapper);
+        ResponseResult responseResult = new PageResponseResult(dto.getPage(),dto.getSize(),(int)page.getTotal());
+        responseResult.setData(page.getRecords());
+        return responseResult;
     }
 }
