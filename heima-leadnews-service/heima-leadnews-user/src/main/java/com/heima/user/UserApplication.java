5f2ad21c563cb0ee25226e31980154147a17d1e2
YIYUANYUAN1116 <571145417@qq.com>
2022-11-03 21:50:23 +0800

    素材列表查询

--- heima-leadnews-service/heima-leadnews-user/src/main/java/com/heima/user/UserApplication.java
+++ heima-leadnews-service/heima-leadnews-user/src/main/java/com/heima/user/UserApplication.java
@@ -1,12 +1,8 @@
 package com.heima.user;
 
-import com.baomidou.mybatisplus.annotation.DbType;
-import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
-import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @MapperScan("com.heima.user.mapper")
@@ -14,5 +10,4 @@ public class UserApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class, args);
     }
-
 }
