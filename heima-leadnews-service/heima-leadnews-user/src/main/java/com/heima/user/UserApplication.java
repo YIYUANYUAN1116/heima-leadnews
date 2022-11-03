3099b30404e8ab8b05f05b59267314ee29aad873
YIYUANYUAN1116 <571145417@qq.com>
2022-11-03 21:49:30 +0800

    素材列表查询

--- heima-leadnews-service/heima-leadnews-user/src/main/java/com/heima/user/UserApplication.java
+++ heima-leadnews-service/heima-leadnews-user/src/main/java/com/heima/user/UserApplication.java
@@ -15,10 +15,4 @@ public class UserApplication {
         SpringApplication.run(UserApplication.class, args);
     }
 
-    @Bean
-    public MybatisPlusInterceptor mybatisPlusInterceptor() {
-        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
-        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
-        return interceptor;
-    }
 }
